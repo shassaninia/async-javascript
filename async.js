f9a12c50809b1528df965632e566fb3c3206f24f
shassaninia <xai.soft80@gmail.com>
2017-02-03 15:22:37 -0500

    generators

--- async.js
+++ async.js
@@ -1,77 +1,62 @@
 window.onload = function () {
+/*
+//Generators are new to ECMAScript 6
 
-    /* Promises */
-    //What is a promise? A promise is an object that represents an 
-    //action that hasn't finished yet, but will do at some point.
-    //It is a placeholder for some asynchronous operation
-    //As soon as an asynchronous operation has started, the Promise
-    //is returnd right away before the data is returned.
-    //We can register callbacks in the promise to handle what happens
-    //when the data comes back.  Promise is a new feature in ECMA Script 6
-    //and isn't supported in all browsers, however, there are libraries such as
-    //q that give more support.
-    //The native promise library is not supported in many browsers
-    //If you want full production support, probably stick to something like
-    //q for now.
-  /*  function get(url) {
-        //resolve is what happens when promise is success
-        //reject is what happens when promise is an error
-        return new Promise(function (resolve, reject) {
-            var xhttp = new XMLHttpRequest();
-            //Open connection
-            xhttp.open("GET", url, true);
-            //If browser supports browser, onload will be supported as
-            //well as onerror. No need to worry about onreadystatechanged
-            xhttp.onload = function () {
-                if (xhttp.status == 200) { //OK
-                    //resolve
-                    resolve(JSON.parse(xhttp.response))
-                } else {
-                    //reject
-                    reject(xhttp.statusText);
-                }
-            };
-            xhttp.onerror = function () {
-                reject(xhttp.statusText);
-            };
-            xhttp.send();
-        });
+//Make a generator, put an * after the funciton keywor
+//We can pause generators by use the yield keyword
+//In this case, javascript will grab the 10, hit the yield and pause hit
+//We can use the genrator by using the .next() method
+//10 is passed outside of the generator, we can use it by using .next();
+function* gen(){
+    var x = yield 10;
+}
+
+//Get the generator ready
+var myGen = gen(); //This does not fire the gen() function
+
+//Run the gen function
+//This will fire the gen() function and
+//return 10 to use
+myGen.next();
+
+//we can pass in data to next, in this case 10 and use it in the gen function
+myGen.next(10);
+*/
+
+genWrap(function*(){
+
+    var tweets = yield $.get("data/tweets.json");
+    console.log(tweets);
+
+    var friends = yield $.get("data/friends.json");
+    console.log(friends);
+
+    var videos = yield $.get("data/videos.json");
+    console.log(videos);
+});
+
+function genWrap(generator){
+   var  gen = generator();
+
+    function handle(yielded){
+        if(!yielded.done){
+            yielded.value.then(function(data){
+                return handle(gen.next(data));
+            })
+        }
     }
 
-    //When get is called, the promise is return to use right away
-    //before the data is returned.
-    var promise = get("data/tweets.json");
+    return handle(gen.next());
+}
 
-    //We can register a callback with this promise to handle the data
-    //by using then.
-    //This basically says once I have the data, then I want to do this, 
-    //in this case console.log the tweets
-    //Chain the catch to the promise to handle errors
-    //We can chain multiple requests together by returning another promise
-    //Once we the data for tweets is returned, console it
-    //Then once the data for friends is returned, console it
-    //Then once the data for videos is returned, console it
-    //
-    promise.then(function (tweets) {
-        console.log(tweets);
-        return get("data/friends.json").then(function (friends) {
-            console.log(friends);
-            return get("data/videos.json").then(function (videos) {
-                console.log(videos);
-            })
-        })
-    }).catch(function (error) {
-        console.log(error)
-    }); */
-
-    //Promises in jquery
-    $.get("data/tweets.json").then(function(tweets){
-        console.log(tweets);
-         return  $.get("data/friends.json");
-    }).then(function(friends){
-        console.log(friends);
-        return $.get("data/videos.json");
-    }).then(function(videos){
-        console.log(videos);
-    })
-};
+// 
+//1. We call handle(gen.next())
+//2. gen.next() has an object with a done property
+//3. Because we have a done property, we can check for it in the handle
+//function yielded.done
+//4. If the generator is not done, get the value from the yielded object
+//5. The value of the object is the promise, so we can use .then()
+//6. We call handle again passingg in the data to gen.next()
+//7. which will put the data into tweets, friends, videos
+//Confusing!!!
+}; 
